63097c80d87ea2d8c0c684f7195b1725488f80a6
Ana Clara <claranogueiramg@gmail.com>
2025-04-29 19:14:35 -0300

    comit inicial ;)

---
+++ src/test/java/com/example/sweetHome/SweetHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sweetHome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
